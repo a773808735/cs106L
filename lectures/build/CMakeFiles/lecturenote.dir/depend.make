1518a374f01c7fc6d7d7ecf7a168475283e2f100
wren <wl3181.nyu.edu>
2023-11-16 18:15:29 -0500

    readme

--- lectures/build/CMakeFiles/lecturenote.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lecturenote.
-# This may be replaced when dependencies are built.
--- lectures/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/C++/CS106L/lectures
-# Build directory: D:/C++/CS106L/lectures/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
